8825dc392d5942c2237f38f594e7fadb77a6e825
Marthinmezaa <juanmameza10@hotmail.com>
2026-01-06 13:34:07 -0300

    Modification 1

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("Sistema de Pruebas Poligráficas iniciado")
+def show_menu():
+    print('\n--- MENU DE PRUEBAS ---')
+    print('[A] Agregar prueba')
+    print('[B] Ver pruebas del dia')
+    print('[C] Ver total a cobrar')
+    print('[D] Exportar a excel')
+    print('[S] Salir')
